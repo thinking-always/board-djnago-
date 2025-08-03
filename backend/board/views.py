847ee4681a12d0246058baa92544f781524a37bc
Jongmin Kim <jongmin3725@gmail.com>
2025-08-03 17:42:08 +1000

    add permission features

--- backend/board/views.py
+++ backend/board/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from .models import Post
-from rest_framework import viewsets
+from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer
+from rest_framework import viewsets, permissions
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by("-created_at")
     serializer_class = PostSerializer
+    permission_classes = [IsAuthorOrReadOnly, permissions.IsAuthenticatedOrReadOnly]
+    
     
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+        
+
     
